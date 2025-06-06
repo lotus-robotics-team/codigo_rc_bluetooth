2e574c3d121e124fad597eaf98db4290d2cd009c
Danilo Fróes <danilofroes.20241@poli.ufrj.br>
2025-06-06 20:54:05 -0300

    UPDATE

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,23 @@
 #include <Arduino.h>
 #include <ps5Controller.h>
 
+const char *ENDERECO_BLUETOOTH = "4c:b9:9b:1c:44:d8";
+
 
 void setup() {
 
+  ps5.begin(ENDERECO_BLUETOOTH);
+  
 }
 
 void loop() {
-  
+
+  if (ps5.isConnected()) {
+    // Loop do robô
+  }
+
+  else {
+    // Robô parado
+  }
+
 }
