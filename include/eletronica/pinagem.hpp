c16ddbc20fc9b5d28e17805c25d19cc901ef7b75
danilofroes <engdanilofroes@gmail.com>
2026-07-25 04:33:23 -0300

    BUILD: Atualizando o tipo de variáveis da pinagem

--- include/eletronica/pinagem.hpp
+++ include/eletronica/pinagem.hpp
@@ -10,12 +10,12 @@
 namespace Pino {
     #ifdef CHIHIRO
         namespace Haku {
-            constexpr gpio_num_t MOTOR_DIREITO_IN1 = GPIO_NUM_18;
-            constexpr gpio_num_t MOTOR_DIREITO_IN2 = GPIO_NUM_19;
+            constexpr gpio_num_t MOTOR_DIREITO_IN1  = GPIO_NUM_18;
+            constexpr gpio_num_t MOTOR_DIREITO_IN2  = GPIO_NUM_19;
             constexpr gpio_num_t MOTOR_ESQUERDO_IN1 = GPIO_NUM_22;
             constexpr gpio_num_t MOTOR_ESQUERDO_IN2 = GPIO_NUM_23;
 
-            constexpr uint8_t SERVO = 25;
+            constexpr std::uint8_t SERVO = 25;
 
             constexpr gpio_num_t LED = GPIO_NUM_26;
         }
