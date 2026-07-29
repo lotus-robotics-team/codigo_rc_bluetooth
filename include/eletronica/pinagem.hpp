3b8b1f1b0eb0cd12ab5c9cb6afeaed13e3c7bc4a
danilofroes <engdanilofroes@gmail.com>
2026-07-29 04:21:43 -0300

    ADD: Pinagem do hockey

--- include/eletronica/pinagem.hpp
+++ include/eletronica/pinagem.hpp
@@ -9,15 +9,28 @@
 
 namespace Pino {
     #ifdef CHIHIRO
-        namespace Haku {
-            constexpr gpio_num_t MOTOR_DIREITO_IN1  = GPIO_NUM_18;
-            constexpr gpio_num_t MOTOR_DIREITO_IN2  = GPIO_NUM_19;
-            constexpr gpio_num_t MOTOR_ESQUERDO_IN1 = GPIO_NUM_22;
-            constexpr gpio_num_t MOTOR_ESQUERDO_IN2 = GPIO_NUM_23;
 
-            constexpr std::uint8_t SERVO = 25;
+        constexpr gpio_num_t MOTOR_DIREITO_IN1  = GPIO_NUM_18;
+        constexpr gpio_num_t MOTOR_DIREITO_IN2  = GPIO_NUM_19;
+        constexpr gpio_num_t MOTOR_ESQUERDO_IN1 = GPIO_NUM_22;
+        constexpr gpio_num_t MOTOR_ESQUERDO_IN2 = GPIO_NUM_23;
 
-            constexpr gpio_num_t LED = GPIO_NUM_26;
-        }
+        constexpr std::uint8_t SERVO = 25;
+
+        constexpr gpio_num_t LED = GPIO_NUM_26;
+
+    #elifdef HOCKEY
+    
+        constexpr gpio_num_t MOTOR_DIREITO_D0  = GPIO_NUM_18;
+        constexpr gpio_num_t MOTOR_DIREITO_D1  = GPIO_NUM_19;
+        constexpr gpio_num_t MOTOR_ESQUERDO_D2 = GPIO_NUM_22;
+        constexpr gpio_num_t MOTOR_ESQUERDO_D3 = GPIO_NUM_23;
+
+    #endif
+}
+
+namespace Canal {
+    #ifdef CHIHIRO
+        constexpr ledc_channel_t SERVO = LEDC_CHANNEL_0;
     #endif
 }
