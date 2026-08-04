542b3e617da7b97dad9b97597f5f73524e15c5f5
danilofroes <engdanilofroes@gmail.com>
2026-08-03 22:01:30 -0300

    ADD; Arquivo de parâmetros (CHIHIRO)

---
+++ include/parametros/chihiro.hpp
@@ -0,0 +1,23 @@
+#ifdef CHIHIRO
+
+#pragma once
+
+#include <cstdint>
+
+float velocidadePadrao = 0.5f;
+float velocidadeAgressiva = 0.7f;
+
+/* MACROS */
+int tempoFreioDeMao = 60;
+
+int tempoGiro45Horario      = 50;
+int tempoGiro45AntiHorario  = 50;
+int tempoGiro90Horario      = 100;
+int tempoGiro90AntiHorario  = 100;
+int tempoGiro180Horario     = 200;
+int tempoGiro180AntiHorario = 200;
+
+int tempoFrenteZigZagHorario     = 100;
+int tempoFrenteZigZagAntiHorario = 100;
+
+#endif
