a0b47bd0cb4e7a4d3d94fe872285ae93c020675b
danilofroes <engdanilofroes@gmail.com>
2026-08-06 01:41:51 -0300

    FIX: Correções na classe da ponte H, uso do ponteiro this-> e setInversao

--- include/eletronica/h_bridge_pwm.hpp
+++ include/eletronica/h_bridge_pwm.hpp
@@ -17,8 +17,6 @@ class HBridgePWM {
         const int pinoIN1;
         const int pinoIN2;
 
-        bool invertido = false;
-
         mcpwm_timer_handle_t timer = nullptr;
 
         mcpwm_oper_handle_t oper = nullptr;
@@ -29,9 +27,8 @@ class HBridgePWM {
         mcpwm_gen_handle_t genIN1 = nullptr;
         mcpwm_gen_handle_t genIN2 = nullptr;
 
-        static constexpr std::uint32_t RESOLUCAO_HZ = 25'000'000;
+        static constexpr std::uint32_t RESOLUCAO_HZ = 20'000'000;
         static constexpr std::uint32_t PERIOD_TICKS = 1000;
-        static constexpr std::uint32_t FREQUENCIA   = RESOLUCAO_HZ / PERIOD_TICKS; // 25MHz
 
         static constexpr std::uint32_t VALOR_MAX_POTENCIA = PERIOD_TICKS;
         static constexpr std::int32_t VALOR_MIN_POTENCIA  = -VALOR_MAX_POTENCIA;
@@ -48,17 +45,15 @@ class HBridgePWM {
                 .intr_priority = 0,                       // Sem prioridade de interrupção
                 .flags = {}
             };
-            ESP_ERROR_CHECK(mcpwm_new_timer(&configTimer, &timer));
-            ESP_ERROR_CHECK(mcpwm_timer_enable(timer));
-            ESP_ERROR_CHECK(mcpwm_timer_start_stop(timer, MCPWM_TIMER_START_NO_STOP));
+            ESP_ERROR_CHECK(mcpwm_new_timer(&configTimer, &this->timer));
         }
 
         void initOperators() {
             mcpwm_operator_config_t configOperator = {};
             configOperator.group_id = 0;
 
-            ESP_ERROR_CHECK(mcpwm_new_operator(&configOperator, &oper));
-            ESP_ERROR_CHECK(mcpwm_operator_connect_timer(oper, timer));
+            ESP_ERROR_CHECK(mcpwm_new_operator(&configOperator, &this->oper));
+            ESP_ERROR_CHECK(mcpwm_operator_connect_timer(this->oper, this->timer));
         }
 
         void initComparators() {
@@ -71,44 +66,44 @@ class HBridgePWM {
                     .update_cmp_on_sync = false
                 }
             };
-            ESP_ERROR_CHECK(mcpwm_new_comparator(oper, &configComparator, &cmprIN1));
-            ESP_ERROR_CHECK(mcpwm_new_comparator(oper, &configComparator, &cmprIN2));
+            ESP_ERROR_CHECK(mcpwm_new_comparator(this->oper, &configComparator, &this->cmprIN1));
+            ESP_ERROR_CHECK(mcpwm_new_comparator(this->oper, &configComparator, &this->cmprIN2));
         }
 
         void initGenerators() {
             mcpwm_generator_config_t configGeneratorIN1 = {
-                .gen_gpio_num = pinoIN1,
+                .gen_gpio_num = this->pinoIN1,
                 .flags = {}
             };
-            ESP_ERROR_CHECK(mcpwm_new_generator(oper, &configGeneratorIN1, &genIN1));
+            ESP_ERROR_CHECK(mcpwm_new_generator(this->oper, &configGeneratorIN1, &this->genIN1));
 
             mcpwm_generator_config_t configGeneratorIN2 = {
-                .gen_gpio_num = pinoIN2,
+                .gen_gpio_num = this->pinoIN2,
                 .flags = {}
             };
-            ESP_ERROR_CHECK(mcpwm_new_generator(oper, &configGeneratorIN2, &genIN2));
+            ESP_ERROR_CHECK(mcpwm_new_generator(this->oper, &configGeneratorIN2, &this->genIN2));
 
             mcpwm_gen_timer_event_action_t actionZero = {
                 .direction = MCPWM_TIMER_DIRECTION_UP,
                 .event = MCPWM_TIMER_EVENT_EMPTY,
                 .action = MCPWM_GEN_ACTION_HIGH
             };
-            ESP_ERROR_CHECK(mcpwm_generator_set_action_on_timer_event(genIN1, actionZero));
-            ESP_ERROR_CHECK(mcpwm_generator_set_action_on_timer_event(genIN2, actionZero));
+            ESP_ERROR_CHECK(mcpwm_generator_set_action_on_timer_event(this->genIN1, actionZero));
+            ESP_ERROR_CHECK(mcpwm_generator_set_action_on_timer_event(this->genIN2, actionZero));
 
             mcpwm_gen_compare_event_action_t actionIN1Compare = {
                 .direction = MCPWM_TIMER_DIRECTION_UP,
-                .comparator = cmprIN1,
+                .comparator = this->cmprIN1,
                 .action = MCPWM_GEN_ACTION_LOW
             };
-            ESP_ERROR_CHECK(mcpwm_generator_set_action_on_compare_event(genIN1, actionIN1Compare));
+            ESP_ERROR_CHECK(mcpwm_generator_set_action_on_compare_event(this->genIN1, actionIN1Compare));
 
             mcpwm_gen_compare_event_action_t actionIN2Compare = {
                 .direction = MCPWM_TIMER_DIRECTION_UP,
-                .comparator = cmprIN2,
+                .comparator = this->cmprIN2,
                 .action = MCPWM_GEN_ACTION_LOW
             };
-            ESP_ERROR_CHECK(mcpwm_generator_set_action_on_compare_event(genIN2, actionIN2Compare));
+            ESP_ERROR_CHECK(mcpwm_generator_set_action_on_compare_event(this->genIN2, actionIN2Compare));
         }
 
     public:
@@ -121,16 +116,18 @@ class HBridgePWM {
         HBridgePWM& operator=(HBridgePWM&&)      = delete; // Desabilita o operador de atribuição de movimentação
 
         ~HBridgePWM() {
-            if (genIN1)  mcpwm_del_generator(genIN1);
-            if (genIN2)  mcpwm_del_generator(genIN2);
-            if (cmprIN1) mcpwm_del_comparator(cmprIN1);
-            if (cmprIN2) mcpwm_del_comparator(cmprIN2);
-            if (oper)    mcpwm_del_operator(oper);
-            if (timer) {
+            if (this->genIN1)  mcpwm_del_generator(this->genIN1);
+            if (this->genIN2)  mcpwm_del_generator(this->genIN2);
+            if (this->cmprIN1) mcpwm_del_comparator(this->cmprIN1);
+            if (this->cmprIN2) mcpwm_del_comparator(this->cmprIN2);
+            if (this->oper)    mcpwm_del_operator(this->oper);
+            if (this->timer) {
                 mcpwm_timer_disable(timer);
                 mcpwm_del_timer(timer);
             }
         }
+
+        bool invertido = false;
 
         inline void begin() {
             initTimer();
@@ -138,30 +135,33 @@ class HBridgePWM {
             initComparators();
             initGenerators();
 
+            ESP_ERROR_CHECK(mcpwm_timer_enable(this->timer));
+            ESP_ERROR_CHECK(mcpwm_timer_start_stop(this->timer, MCPWM_TIMER_START_NO_STOP));
+
             freiar();
         }
 
         inline void setPotencia(std::int32_t p) {
             p = std::clamp<std::int32_t>(p, VALOR_MIN_POTENCIA, VALOR_MAX_POTENCIA);
 
-            if (invertido) p = -p;
+            if (this->invertido) p = -p;
 
             if (p > 0) {
-                mcpwm_comparator_set_compare_value(cmprIN1, VALOR_MAX_POTENCIA - p);
-                mcpwm_comparator_set_compare_value(cmprIN2, VALOR_MAX_POTENCIA);
+                mcpwm_comparator_set_compare_value(this->cmprIN1, p);
+                mcpwm_comparator_set_compare_value(this->cmprIN2, 0);
             }
 
             else if (p < 0) {
-                mcpwm_comparator_set_compare_value(cmprIN1, VALOR_MAX_POTENCIA);
-                mcpwm_comparator_set_compare_value(cmprIN2, VALOR_MAX_POTENCIA + p);
+                mcpwm_comparator_set_compare_value(this->cmprIN1, 0);
+                mcpwm_comparator_set_compare_value(this->cmprIN2, -p);
             }
 
             else {
-                mcpwm_comparator_set_compare_value(cmprIN1, VALOR_MAX_POTENCIA);
-                mcpwm_comparator_set_compare_value(cmprIN2, VALOR_MAX_POTENCIA);
+                mcpwm_comparator_set_compare_value(this->cmprIN1, VALOR_MAX_POTENCIA);
+                mcpwm_comparator_set_compare_value(this->cmprIN2, VALOR_MAX_POTENCIA);
             }
 
-            potencia = p;
+            this->potencia = p;
         }
 
         inline void freiar() {
@@ -171,6 +171,13 @@ class HBridgePWM {
         inline void pontoMorto() {
             mcpwm_comparator_set_compare_value(cmprIN1, 0);
             mcpwm_comparator_set_compare_value(cmprIN2, 0);
+            this->potencia = 0;
+        }
+
+        inline void setInversao(bool inversao) {
+            if (inversao == this->invertido) return;
+
+            this->invertido = inversao;
         }
 
         [[nodiscard]] inline std::int32_t getPotencia() const {
